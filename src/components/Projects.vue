28ad9c8c612f3b0572e8b198505ebed0ebdd4e29
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-20 11:05:03 +0200

    Add gifs to projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,7 +3,7 @@
         <header class="font-bold text-left text-2xl mb-2">Projects</header>
         <div class="flex md:grid grid-cols-2 gap-6">
             <div class="p-2 leading-loose relative shadow">
-                <a class="" href="https://github.com/chadriae/king-size"><img class="h-80" @mouseover="hover = true" @mouseleave="hover = false" :src="chained"></a>
+                <a class="" href="https://github.com/chadriae/king-size"><img class="h-80" @mouseover="chainedHover = true" @mouseleave="chainedHover = false" :src="chained"></a>
                 <header class="uppercase font-semibold">Chained</header>
                 <p class="text-gray-700 h-32">A website made for people who want to look for a local bicycle repairer. Repairers also get to make an account and share their specialties and locality.</p>
                 <div class="absolute bottom-2 inset-x-4 flex flex-row-reverse gap-2">
@@ -13,7 +13,7 @@
                 </div>
              </div>
             <div class="p-2 leading-loose relative shadow">
-                <a class="" href="https://github.com/chadriae/php-mvc-Ismi"><img src="../assets/beconnect.png" class=""></a>
+                <a class="" href="https://github.com/chadriae/php-mvc-Ismi"><img class="h-80" @mouseover="beconnectHover = true" @mouseleave="beconnectHover = false" :src="beconnect"></a>
                 <header class="uppercase font-semibold">BeConnect</header>
                 <p class="text-gray-700 h-32">A place where BeCode-alumni could meet, register, update their profiles and talk about their experiences.</p>
                 <div class="absolute bottom-2 inset-x-4 flex flex-row-reverse gap-2">
@@ -22,7 +22,7 @@
                 </div>
             </div>
             <div class="p-2 leading-loose relative shadow">
-                <a class="" href="https://github.com/chadriae/intro-react"><img src="../assets/todo.png" class=""></a>
+                <a class="" href="https://github.com/chadriae/intro-react"><img class="h-80" @mouseover="todoHover = true" @mouseleave="todoHover = false" :src="todo"></a>
                 <header class="uppercase font-semibold">ToDo</header>
                 <p class="text-gray-700 h-32">Simple and fun todo-app made with React. Added time, date and inspirational quotes.</p>
                 <div class="absolute bottom-2 inset-x-4 flex flex-row-reverse gap-2">
@@ -38,19 +38,39 @@
 export default {
       data () {
       return {
-          chainedStatic: require("../assets/chained.png"),
-          chainedGif: require("../assets/chained.gif"),
-          hover: false
+        chainedHover: false,
+        beconnectHover: false,
+        todoHover: false,
+        chainedStatic: require("../assets/chained.png"),
+        chainedGif: require("../assets/chained.gif"),
+        beconnectStatic: require("../assets/beconnect.png"),
+        beconnectGif: require("../assets/beconnect.gif"),
+        todoStatic: require("../assets/todo.png"),
+        todoGif: require("../assets/todo.gif"),
       }
   },
   computed: {
-      chained: function () {
-           if (this.hover) {
-                return this.chainedGif;
-            } else {
-                return this.chainedStatic;
-            }
-      }
+    chained: function () {
+        if (this.chainedHover) {
+            return this.chainedGif;
+        } else {
+            return this.chainedStatic;
+        }
+    },
+    beconnect: function () {
+        if (this.beconnectHover) {
+            return this.beconnectGif;
+        } else {
+            return this.beconnectStatic;
+        }
+    },
+    todo: function () {
+        if (this.todoHover) {
+            return this.todoGif;
+        } else {
+            return this.todoStatic;
+        }
+    }
   }
 }
 </script>
