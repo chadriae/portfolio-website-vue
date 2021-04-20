8585924c8958b5f1b2c4ab45f4fc24c3b777fabc
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-20 12:29:01 +0200

    Add dev.to logo and link

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="mt-24 grid grid-cols-2">
-        <div class="">
-
+    <div class="mt-24 grid grid-cols-2 justify-items-stretch">
+        <div class="text-container">
             <div class="mb-12 mr-4">
                 <header class="font-bold text-left text-2xl mb-2">About me</header>
                 <p class="text-left text-gray-700 leading-relaxed p-2">
@@ -22,11 +21,12 @@
             <div class="mt-4 p-2 flex gap-4">
                 <a href="https://github.com/chadriae"><img src="../assets/github.png" class="h-12 social-media"></a>
                 <a href="https://www.linkedin.com/in/christophe-adriaensens/"><img src="../assets/linkedin.png" class="h-12 social-media"></a>
+                <a href="https://dev.to/chadriae"><img src="../assets/devto.png" class="h-12 social-media rounded-full"></a>
                 <a href="https://twitter.com/chadriae"><img src="../assets/twitter.png" class="h-12 social-media"></a>
             </div>
         </div>
-        <div class="pic-container h-96 w-72 m-2">
-            <img class="pic h-96 w-auto" @mouseover="hover = true" @mouseleave="hover = false" :src="image">
+        <div class="pic-container h-96 w-72 mt-8 mr-4 justify-self-end">
+            <div class="justify-self-end"><img class="pic h-96 w-auto" @mouseover="hover = true" @mouseleave="hover = false" :src="image"></div>
         </div>
     </div>
 </template>
