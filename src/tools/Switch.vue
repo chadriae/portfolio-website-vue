d5116cf01a2ef6937d903054ee84be0ad1351693
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-26 16:49:17 +0200

    Remove outline on switch button

--- src/tools/Switch.vue
+++ src/tools/Switch.vue
@@ -12,12 +12,12 @@
     <Switch
       v-model="enabled"
       :class="enabled ? 'background-white' : 'background-dark'"
-      class="relative inline-flex items-center h-6 rounded-full w-11 border border-black"
+      class="focus:outline-none relative inline-flex items-center h-6 rounded-full w-11 border border-black"
       @click="$emit('toggle')"
     >
       <span
         :class="enabled ? 'transition duration-700 translate-x-1 bg-yellow-500' : 'transition duration-700 translate-x-6 bg-yellow-200'"
-        class="inline-block w-4 h-4 transform rounded-full"
+        class="focus:outline-none inline-block w-4 h-4 transform rounded-full"
       />
     </Switch>
 
