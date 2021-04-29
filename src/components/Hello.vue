c92df62fcd45c74e5b988350b3ce0c6bbc4c0916
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2021-04-29 15:49:37 +0200

    Font size name fix responsiveness

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-left">
-        <header id="name" class="roboto uppercase font-bold text-5xl mb-2" :class="(mode === 'dark') ? 'grey-font-color' : 'blue-font-color'">Christophe Adriaensens</header>
+        <header id="name" class="roboto uppercase font-bold mb-2" :class="(mode === 'dark') ? 'grey-font-color' : 'blue-font-color'">Christophe Adriaensens</header>
         <p class="roboto font-bold text-xl mb-2" :class="(mode === 'dark') ? 'dark-border-color' : 'blackish-font-color'">Junior full-stack developer</p>
         <p class="text-gray-700 ml-2" :class="(mode === 'dark') ? 'grey-font-color' : 'blackish-font-color'">Based in Ghent</p>
     </div>
@@ -17,6 +17,8 @@ export default {
 
 #name {
     text-shadow: -2px 0px 0px rgb(167, 167, 167), 2px 0px 0px $pic-hover-color;
+    font-size: 2.5rem;
+    line-height: 2.5rem;   
 }
 
 .roboto {
